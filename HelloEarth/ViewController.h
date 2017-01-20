62a79e16b85ab7cecd7d00b2b6cefab6c424bfdb
Dmytro Yaropovetsky <dmytro.yaropovetsky@rocketroute.com>
2017-01-20 18:33:42 +0200

    Initial Commit

---
+++ HelloEarth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloEarth
+//
+//  Created by Dmytro Yaropovetsky on 1/20/17.
+//  Copyright © 2017 yar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
